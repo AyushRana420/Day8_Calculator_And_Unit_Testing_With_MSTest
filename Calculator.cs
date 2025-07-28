3a8873217cc388f48820c458b5a6343c98d7df57
Ayush <ayushrana419@gmail.com>
2025-07-28 14:09:37 +0530

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Calculator
+{
+    public class Calculator
+    {
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public double Divide(int a, int b)
+        {
+           return (double)a / b;
+        }
+    }
+}
